4cf22f105b1f8e00c9155b2d7677455adf5b8b6c
surenbthapa <surenthapa5803@gmail.com>
2019-11-24 17:51:05 +0530

    Uploaded codes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# tweetsentiment
+# Introduction
+
+### A simple project on sentiment analysis.
+### Semester-5, Software Engineering,
+### Delhi Technological University, Delhi-110042, India
---
+++ twitter_credentials.py
@@ -0,0 +1,9 @@
+#Variables that contain the user credentials to acess Twitter API
+ACCESS_TOKEN="**********************************************"
+ACCESS_TOKEN_SECRET="**********************************************"
+CONSUMER_KEY="**********************************************"
+CONSUMER_SECRET="**********************************************"
+
+#The keys are hidden because of security reasons.
+
+#Get this by going to apps.twitter.com and getting the tokens for selected app
